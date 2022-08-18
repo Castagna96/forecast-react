601655ffad943681f3f3bcfebd0919a94315506f
Franco Castagna <franco_castagna@hotmail.com>
2022-08-18 18:00:45 -0300

    API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react'
 import './App.css';
+import Weather from './components/data.js'
 
 function App() {
   return (
-    <div className="App">
+    <div className='app-container'>
+        <h1 className='app-heading'>Weather App</h1>
+        <Weather/>
     </div>
   );
 }
